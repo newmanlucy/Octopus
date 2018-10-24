faae38c7005f561ddab2e250aa8904f1671f734a
Chaihyun Lee <clee0505@uchicago.edu>
2018-10-23 23:47:00 -0500

    just saving old stuff in case

--- model.py
+++ model.py
@@ -27,71 +27,102 @@ class Model:
     def run_model(self):
         
         # Get weights and biases
-        with tf.variable_scope('encoder'):
-            self.W_in = tf.get_variable('W_in', initializer=par['W_in_init'], trainable=True)
-            self.W_enc = tf.get_variable('W_enc', initializer=par['W_enc_init'], trainable=True)
-            self.b_latent = tf.get_variable('b_latent', initializer=par['b_latent_init'], trainable=True)
-            self.W_link = tf.get_variable('W_link', initializer=par['W_link_init'], trainable=True)
-            self.b_link = tf.get_variable('b_link', initializer=par['b_link_init'], trainable=True)
+        if par['num_layers'] == 5:
+            with tf.variable_scope('encoder'):
+                self.W_in = tf.get_variable('W_in', initializer=par['W_in_init'], trainable=True)
+                self.b_enc = tf.get_variable('b_enc', initializer=par['b_enc_init'], trainable=True)
+                self.W_enc = tf.get_variable('W_enc', initializer=par['W_enc_init'], trainable=True)
+                self.b_latent = tf.get_variable('b_latent', initializer=par['b_latent_init'], trainable=True)
+                self.W_link = tf.get_variable('W_link', initializer=par['W_link_init'], trainable=True)
+                self.b_link = tf.get_variable('b_link', initializer=par['b_link_init'], trainable=True)
 
-        with tf.variable_scope('decoder'):
-            self.W_dec = tf.get_variable('W_dec', initializer=par['W_dec_init'], trainable=True)
-            self.b_dec = tf.get_variable('b_dec', initializer=par['b_dec_init'], trainable=True)
-            self.W_link2 = tf.get_variable('W_link2', initializer=par['W_link2_init'], trainable=True)
-            self.b_link2 = tf.get_variable('b_link2', initializer=par['b_link2_init'], trainable=True)
-            self.W_out = tf.get_variable('W_out', initializer=par['W_out_init'], trainable=True)
-            self.b_out = tf.get_variable('b_out', initializer=par['b_out_init'], trainable=True)
+            with tf.variable_scope('decoder'):
+                self.W_dec = tf.get_variable('W_dec', initializer=par['W_dec_init'], trainable=True)
+                self.b_dec = tf.get_variable('b_dec', initializer=par['b_dec_init'], trainable=True)
+                self.W_link2 = tf.get_variable('W_link2', initializer=par['W_link2_init'], trainable=True)
+                self.b_link2 = tf.get_variable('b_link2', initializer=par['b_link2_init'], trainable=True)
+                self.W_out = tf.get_variable('W_out', initializer=par['W_out_init'], trainable=True)
+                self.b_out = tf.get_variable('b_out', initializer=par['b_out_init'], trainable=True)
+        
+        elif par['num_layers'] == 3:
+            with tf.variable_scope('encoder'):
+                self.W_in = tf.get_variable('W_in', initializer=par['W_in_init'], trainable=True)
+                self.b_enc = tf.get_variable('b_enc', initializer=par['b_enc_init'], trainable=True)
+                self.W_enc = tf.get_variable('W_enc', initializer=par['W_enc_init'], trainable=True)
+                self.b_latent = tf.get_variable('b_latent', initializer=par['b_latent_init'], trainable=True)
+
+            with tf.variable_scope('decoder'):
+                self.W_dec = tf.get_variable('W_dec', initializer=par['W_dec_init'], trainable=True)
+                self.b_dec = tf.get_variable('b_dec', initializer=par['b_dec_init'], trainable=True)
+                self.W_out = tf.get_variable('W_out', initializer=par['W_out_init'], trainable=True)
+                self.b_out = tf.get_variable('b_out', initializer=par['b_out_init'], trainable=True)
+
+        elif par['num_layers'] == 2:
+            with tf.variable_scope('encoder'):
+                self.W_in = tf.get_variable('W_in', initializer=par['W_in_init'], trainable=True)
+                self.b_enc = tf.get_variable('b_enc', initializer=par['b_enc_init'], trainable=True)
+
+            with tf.variable_scope('decoder'):
+                self.W_dec = tf.get_variable('W_dec', initializer=par['W_dec_init'], trainable=True)
+                self.b_dec = tf.get_variable('b_dec', initializer=par['b_dec_init'], trainable=True)
+                self.W_out = tf.get_variable('W_out', initializer=par['W_out_init'], trainable=True)
+                self.b_out = tf.get_variable('b_out', initializer=par['b_out_init'], trainable=True)
 
         # Run input through the model layers
-        self.enc = tf.nn.relu(tf.matmul(self.input_data, self.W_in))
-        self.link = tf.nn.relu(tf.matmul(self.enc, self.W_enc) + self.b_latent)
-        self.latent = tf.nn.relu(tf.matmul(self.link, self.W_link) + self.b_link)
-        self.link2 = tf.nn.relu(tf.matmul(self.latent, self.W_dec) + self.b_dec)
-        self.dec = tf.nn.relu(tf.matmul(self.link2, self.W_link2) + self.b_link2)
-        self.output = tf.matmul(self.dec, self.W_out) + self.b_out
-
+        if par['num_layers'] == 5:
+            self.enc = tf.nn.sigmoid(tf.add(tf.matmul(self.input_data, self.W_in), self.b_enc))
+            self.link = tf.nn.sigmoid(tf.add(tf.matmul(self.enc, self.W_enc), self.b_latent))
+            self.latent = tf.nn.sigmoid(tf.add(tf.matmul(self.link, self.W_link), self.b_link))
+            self.link2 = tf.nn.sigmoid(tf.add(tf.matmul(self.latent, self.W_dec), self.b_dec))
+            self.dec = tf.nn.sigmoid(tf.add(tf.matmul(self.link2, self.W_link2), self.b_link2))
+            self.output = tf.nn.relu(tf.add(tf.matmul(self.dec, self.W_out), self.b_out))
+        
+        elif par['num_layers'] == 3:
+            self.enc = tf.nn.sigmoid(tf.add(tf.matmul(self.input_data, self.W_in), self.b_enc))
+            self.latent = tf.nn.sigmoid(tf.add(tf.matmul(self.enc, self.W_enc), self.b_latent))
+            self.dec = tf.nn.sigmoid(tf.add(tf.matmul(self.latent, self.W_dec), self.b_dec))
+            self.output = tf.nn.relu(tf.add(tf.matmul(self.dec, self.W_out), self.b_out))
+        
+        elif par['num_layers'] == 2:
+            self.enc = tf.nn.sigmoid(tf.add(tf.matmul(self.input_data, self.W_in), self.b_enc))
+            self.dec = tf.nn.sigmoid(tf.add(tf.matmul(self.enc, self.W_dec), self.b_dec))
+            self.output = tf.nn.relu(tf.add(tf.matmul(self.dec, self.W_out), self.b_out))
 
         # # LITERATURE STUFF
-        if par['use_literature_code']:
-            n_nodes_inpl = 128*128
-            n_nodes_hl1 = 138
-            n_nodes_hl2 = 138
-            n_nodes_outl = 128*128
+        # if par['use_literature_code']:
+        #     n_nodes_inpl = 128*128
+        #     n_nodes_hl1 = par['n_hidden']
+        #     n_nodes_hl2 = par['n_hidden']
+        #     n_nodes_outl = 128*128
 
-            hidden_1_layer_vals = {
-              'weights':tf.Variable(tf.random_normal([n_nodes_inpl,n_nodes_hl1])),
-              'biases':tf.Variable(tf.random_normal([n_nodes_hl1]))  }
+        #     hidden_1_layer_vals = {
+        #       'weights':tf.Variable(tf.random_normal([n_nodes_inpl,n_nodes_hl1])),
+        #       'biases':tf.Variable(tf.random_normal([n_nodes_hl1]))  }
                 
-            # second hidden layer has 32*32 weights and 32 biases
-            hidden_2_layer_vals = {
-              'weights':tf.Variable(tf.random_normal([n_nodes_hl1, n_nodes_hl2])),
-              'biases':tf.Variable(tf.random_normal([n_nodes_hl2]))  }
+        #     # second hidden layer has 32*32 weights and 32 biases
+        #     hidden_2_layer_vals = {
+        #       'weights':tf.Variable(tf.random_normal([n_nodes_hl1, n_nodes_hl2])),
+        #       'biases':tf.Variable(tf.random_normal([n_nodes_hl2]))  }
         
-            # second hidden layer has 32*784 weights and 784 biases
-            output_layer_vals = {
-              'weights':tf.Variable(tf.random_normal([n_nodes_hl2,n_nodes_outl])),
-                'biases':tf.Variable(tf.random_normal([n_nodes_outl])) }
+        #     # second hidden layer has 32*784 weights and 784 biases
+        #     output_layer_vals = {
+        #       'weights':tf.Variable(tf.random_normal([n_nodes_hl2,n_nodes_outl])),
+        #         'biases':tf.Variable(tf.random_normal([n_nodes_outl])) }
 
             
-            layer1 = tf.nn.sigmoid(tf.add(tf.matmul(self.input_data, hidden_1_layer_vals['weights']), hidden_1_layer_vals['biases']))
-            layer2 = tf.nn.sigmoid(tf.add(tf.matmul(layer1, hidden_2_layer_vals['weights']), hidden_2_layer_vals['biases']))
-            output_layer = tf.nn.relu(tf.add(tf.matmul(layer2, output_layer_vals['weights']), output_layer_vals['biases']))
-            self.output = output_layer
+        #     layer1 = tf.nn.sigmoid(tf.add(tf.matmul(self.input_data, hidden_1_layer_vals['weights']), hidden_1_layer_vals['biases']))
+        #     layer2 = tf.nn.sigmoid(tf.add(tf.matmul(layer1, hidden_2_layer_vals['weights']), hidden_2_layer_vals['biases']))
+        #     if par['normalize']:
+        #         output_layer = tf.matmul(layer2, output_layer_vals['weights']) + output_layer_vals['biases']
+        #     else:
+        #         output_layer = tf.nn.relu(tf.add(tf.matmul(layer2, output_layer_vals['weights']), output_layer_vals['biases']))
+        #     self.output = output_layer
 
     def optimize(self):
         # Calculae loss
         self.loss = tf.reduce_mean(tf.square(self.target_data - self.output))
+        self.train_op = tf.train.AdagradOptimizer(par['learning_rate']).minimize(self.loss)
 
-        # Use Adam Optimizer to train
-        opt = tf.train.AdamOptimizer(learning_rate = par['learning_rate'])
-        grads_and_vars = opt.compute_gradients(self.loss)
-        self.train_op = opt.apply_gradients(grads_and_vars)
-
-
-        # # LITERATURE STUFF
-        if par['use_literature_code']:
-            learn_rate = 0.1   # how fast the model should learn
-            self.train_op = tf.train.AdagradOptimizer(learn_rate).minimize(self.loss)
 
 def main():
 
@@ -120,11 +151,6 @@ def main():
 
             # Generate training set
             input_data, target_data = stim.generate_train_batch()
-            if par['normalize']:
-                mean = input_data.mean()
-                std = input_data.std()
-                input_data = (input_data - mean) / std
-                target_data = input_data
             feed_dict = {x: input_data, y: target_data}
             _, train_loss, model_output = sess.run([model.train_op, model.loss, model.output], feed_dict=feed_dict)
 
@@ -137,12 +163,18 @@ def main():
 
                 # Save one training and output img from this iteration
                 if i % par['save_iter'] == 0:
-                    original = input_data[0].reshape(par['img_shape'])
-                    output = model_output[0].reshape(par['img_shape'])
+                    original1 = input_data[0].reshape(par['img_shape'])
+                    output1 = model_output[0].reshape(par['img_shape'])
+                    original2 = input_data[1].reshape(par['img_shape'])
+                    output2 = model_output[1].reshape(par['img_shape'])
+                    original3 = input_data[2].reshape(par['img_shape'])
+                    output3 = model_output[2].reshape(par['img_shape'])
                 
-                    vis = np.concatenate((original, output), axis=1)
-                    if par['normalize']:
-                        vis = vis * std + mean
+                    vis1 = np.concatenate((original1, output1), axis=1)
+                    vis2 = np.concatenate((original2, output2), axis=1)
+                    vis3 = np.concatenate((original3, output3), axis=1)
+                    vis = np.concatenate((vis1, vis2), axis=0)
+                    vis = np.concatenate((vis, vis3), axis=0)
                     cv2.imwrite(par['save_dir']+'run_'+str(par['run_number'])+'_test_'+str(i)+'.png', vis)
 
                 # Plot loss curve
--- parameters.py
+++ parameters.py
@@ -7,81 +7,107 @@ print("Loading parameters...")
 Set independent parameters
 """
 par = {
-	# Setup parameters
-	'save_dir'			: './savedir/',
-	'img_dir'			: './bw_im/',
-	'img_size'			: 32,
+    # Setup parameters
+    'save_dir'          : './savedir/',
+    'img_dir'           : './bw_im/',
+    'img_size'          : 32,
 
-	# Network shape
-	'n_enc'				: 125,
-	'n_link'			: 100,
-	'n_latent'			: 75,
-	'n_dec'				: 125,
-	
-	# Training
-	'task'				: 'bw_to_bw_simple',
-	'learning_rate'		: 5e-2,
-	'connection_prob'	: 1,
+    # Network shape
+    'n_enc'             : 125,
+    'n_link'            : 100,
+    'n_latent'          : 75,
+    'n_dec'             : 125,
+    'num_layers'        : 3,
+    'new_model'         : False,
+    
+    # Training
+    'task'              : 'bw_to_bw_simple',
+    'learning_rate'     : 5e-2,
+    'connection_prob'   : 1,
 
-	# Variance
-	'input_mean'		: 0.0,
-	'noise_in_sd'		: 0.1,
-	'noise_rnn_sd'		: 0.5,
+    # Variance
+    'input_mean'        : 0.0,
+    'noise_in_sd'       : 0.1,
+    'noise_rnn_sd'      : 0.5,
 
-	# Training setup
-	'batch_train_size'	: 256,
-	'num_iterations'	: 50001,
-	'print_iter'		: 100,
+    # Training setup
+    'batch_train_size'  : 256,
+    'num_iterations'    : 50001,
+    'print_iter'        : 100,
 }
 
 """
 Set dependent parameters
 """
 def update_dependencies():
-	print('Updating dependencies...\n')
+    print('Updating dependencies...\n')
 
-	if par['task'] == 'bw_to_bw':
-		par['data_dir'] = './bw_im'
-		par['img_shape'] = (par['img_size'],par['img_size'],3)
-		par['n_input'] = par['img_size']*par['img_size']*3
-		par['n_output'] = par['n_input']
+    if par['task'] == 'bw_to_bw':
+        par['data_dir'] = './bw_im'
+        par['img_shape'] = (par['img_size'],par['img_size'],3)
+        par['n_input'] = par['img_size']*par['img_size']*3
+        par['n_output'] = par['n_input']
 
-	elif par['task'] == 'bw_to_bw_simple':
-		par['data_dir'] = './bw_im'
-		par['img_shape'] = (par['img_size'],par['img_size'])
-		par['n_input'] = par['img_size']*par['img_size']
-		par['n_output'] = par['n_input']
+    elif par['task'] == 'bw_to_bw_simple':
+        par['data_dir'] = './bw_im'
+        par['img_shape'] = (par['img_size'],par['img_size'])
+        par['n_input'] = par['img_size']*par['img_size']
+        par['n_output'] = par['n_input']
 
-	# Set up initializers
-	c = 0.1
-	par['W_in_init'] = c*np.float32(np.random.gamma(shape=0.25, scale=1.0, size = [par['n_input'], par['n_enc']]))
-	
-	par['W_enc_init'] = c*np.float32(np.random.gamma(shape=0.25, scale=1.0, size = [par['n_enc'], par['n_link']]))
-	par['b_latent_init'] = np.zeros((1,par['n_link']), dtype = np.float32)
+    # Set up initializers
+    if par['num_layers'] == 5:
+        par['W_in_init'] = np.random.normal(size=[par['n_input'], par['n_enc']])
+        par['b_enc_init'] = np.random.normal(size=(1,par['n_enc']))
 
-	par['W_link_init'] = c*np.float32(np.random.gamma(shape=0.25, scale=1.0, size = [par['n_link'], par['n_latent']]))
-	par['b_link_init'] = np.zeros((1,par['n_latent']), dtype = np.float32)
-	
-	par['W_dec_init'] = c*np.float32(np.random.gamma(shape=0.25, scale=1.0, size = [par['n_latent'], par['n_link']]))
-	par['b_dec_init'] = np.zeros((1,par['n_link']), dtype = np.float32)
+        par['W_enc_init'] = np.random.normal(size=[par['n_enc'], par['n_link']])
+        par['b_latent_init'] = np.random.normal((1,par['n_link']))
 
-	par['W_link2_init'] = c*np.float32(np.random.gamma(shape=0.25, scale=1.0, size = [par['n_link'], par['n_dec']]))
-	par['b_link2_init'] = np.zeros((1,par['n_dec']), dtype = np.float32)
+        par['W_link_init'] = np.random.normal(size=[par['n_link'], par['n_latent']])
+        par['b_link_init'] = np.random.normal((1,par['n_latent']))
+        
+        par['W_dec_init'] = np.random.normal(size = [par['n_latent'], par['n_link']])
+        par['b_dec_init'] = np.random.normal((1,par['n_link']))
 
-	par['W_out_init'] = np.float32(np.random.uniform(-c, c, size = [par['n_dec'], par['n_output']]))
-	par['b_out_init'] = np.zeros((1,par['n_output']), dtype = np.float32)
+        par['W_link2_init'] = np.random.normal(size=[par['n_link'], par['n_dec']])
+        par['b_link2_init'] = np.random.normal((1,par['n_dec']))
+
+        par['W_out_init'] = np.random.normal(size=[par['n_dec'], par['n_output']])
+        par['b_out_init'] = np.random.normal((1,par['n_output']))
+    
+    elif par['num_layers'] == 3:
+        par['W_in_init'] = np.random.normal(size=[par['n_input'], par['n_enc']])
+        par['b_enc_init'] = np.random.normal(size=(1,par['n_enc']))
+
+        par['W_enc_init'] = np.random.normal(size=[par['n_enc'], par['n_latent']])
+        par['b_latent_init'] = np.random.normal((1,par['n_latent']))
+        
+        par['W_dec_init'] = np.random.normal(size=[par['n_latent'], par['n_dec']])
+        par['b_dec_init'] = np.random.normal((1,par['n_dec']))
+
+        par['W_out_init'] = np.random.normal(size=[par['n_dec'], par['n_output']])
+        par['b_out_init'] = np.random.normal((1,par['n_output']))
+    
+    elif par['num_layers'] == 2:
+        par['W_in_init'] = np.float32(np.random.normal(size=[par['n_input'], par['n_enc']]))
+        par['b_enc_init'] = np.float32(np.random.normal(size=(1,par['n_enc'])))
+
+        par['W_dec_init'] = np.float32(np.random.normal(size=[par['n_enc'], par['n_dec']]))
+        par['b_dec_init'] = np.float32(np.random.normal(size=(1,par['n_dec'])))
+
+        par['W_out_init'] = np.float32(np.random.normal(size=[par['n_dec'], par['n_output']]))
+        par['b_out_init'] = np.float32(np.random.normal(size=(1,par['n_output'])))
 
 """
 Update parameters based on the given dictionary (updates)
 """
 def update_parameters(updates):
-	print('Updating parameters...')
+    print('Updating parameters...')
 
-	for (key, val) in updates.items():
-		par[key] = val
+    for (key, val) in updates.items():
+        par[key] = val
 
-	# Update any dependent parameters
-	update_dependencies()
+    # Update any dependent parameters
+    update_dependencies()
 
 
 # Run update dependencies when loading parameters
