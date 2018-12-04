43ec012813091edca8cae59b37fc03eb997e52c6
Lucy Newman <newmanlucy@uchicago.edu>
2018-12-03 21:16:54 -0600

    running conv with video

--- video.py
+++ video.py
@@ -19,11 +19,7 @@ from best_evo import EvoModel
 # Ignore tensorflow warning
 os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 
-def run_model(input_data, conv_target, evo_target, gpu_id = None):
-
-    if gpu_id is not None:
-        os.environ["CUDA_VISIBLE_DEVICES"] = ""
-        gpu_id = None
+def load_model():
 
     # Reset Tensorflow graph
     tf.reset_default_graph()
@@ -39,79 +35,70 @@ def run_model(input_data, conv_target, evo_target, gpu_id = None):
     evo_model.update_variables(best_weights)
     print('Loaded evo model')
 
-    # Model stats
-    losses = []
-    testing_losses = []
-
     config = tf.ConfigProto(allow_soft_placement=True)
-    with tf.Session(config=config) as sess:
+    sess = tf.Session(config=config)
+    init = tf.global_variables_initializer()
+    sess.run(init)
 
-        init = tf.global_variables_initializer()
-        sess.run(init)
+    folder = './latent_all_img_batch16_filt16_loss150/'
+    conv_model = tf.train.import_meta_graph(folder + 'conv_model_with_latent.meta', clear_devices=True)
+    conv_model.restore(sess, tf.train.latest_checkpoint(folder)) 
+    print('Loaded conv model from',folder)
 
-        device = '/cpu:0' if gpu_id is None else '/gpu:0'
-        with tf.device(device):
-            folder = './latent_all_img_batch16_filt16_loss150/'
-            conv_model = tf.train.import_meta_graph(folder + 'conv_model_with_latent.meta', clear_devices=True)
-            conv_model.restore(sess, tf.train.latest_checkpoint(folder)) 
-            print('Loaded conv model from',folder)
+    return stim, evo_model, sess
 
-            # Generate batch from testing set and check the output
-            # (16,128,128), (16,128,128,3) bw, (16,128,128,3) color
-            input_data, conv_target, evo_target = stim.generate_test_batch()
-            # for i in range(10):
-                # start = time.time()
-            feed_dict = {'x:0': input_data, 'y:0': conv_target}
-            test_loss, conv_output, encoded = sess.run(['l:0', 'o:0','encoded:0'], feed_dict=feed_dict)
+def run_model(stim, evo_model, sess, input_data, conv_target, evo_target):    
 
-            # "TEST" EVO MODEL
-            evo_model.load_batch(encoded, evo_target)
-            evo_model.run_models()
-            evo_model.judge_models()
+    # Generate batch from testing set and check the output
+    # (16,128,128), (16,128,128,3) bw, (16,128,128,3) color
+    input_data, conv_target, evo_target = stim.generate_test_batch()
+    # for i in range(10):
+        # start = time.time()
+    feed_dict = {'x:0': input_data, 'y:0': conv_target}
+    test_loss, conv_output, encoded = sess.run(['l:0', 'o:0','encoded:0'], feed_dict=feed_dict)
+    
 
-            evo_output = evo_model.output
-            test_loss = evo_model.get_losses(True)
-            testing_losses.append(test_loss[0])
+    # "TEST" EVO MODEL
+    evo_model.load_batch(encoded, evo_target)
+    evo_model.run_models()
+    evo_model.judge_models()
+
+    evo_output = evo_model.output
+    test_loss = evo_model.get_losses(True)
+    print('Evo loss:',test_loss)
+    print((evo_output.shape, conv_output.shape))
     return evo_output
                 # end = time.time()
                 # print(end-start)
             # display evo_output[0]
 
-def actually_run_model(input_data, conv_target, evo_target):
+def actually_run_model(stim, evo_model, sess, input_data, conv_target, evo_target):
     # Run Model
     t0 = time.time()
     try:
-        updates = {
-            'a_note'            : 'latent to evo, all img',
-            'input_dir'         : './bw_im/',
-            'target_dir'        : './raw_im/',
-            'print_iter'        : 1,
-            'save_iter'         : 5,
-            'batch_train_size'  : 16,
-            'run_number'        : 14,
-            'num_conv1_filters' : 16,
-            'n_networks'        : 65,
-            'survival_rate'     : 0.12,
-            'mutation_rate'     : 0.6,
-            'mutation_strength' : 0.45,
-            'migration_rate'    : 0.1,
-            'task'              : 'conv_task',
-            'one_img'           : False,
-            'simulation'        : False
-        }
-        update_parameters(updates)
-        print('Model number ' + str(par['run_number']) + ' running!')
-        run_model(input_data, conv_target, evo_target)
+        # print('Model number ' + str(par['run_number']) + ' running!')
+        evo_output = run_model(stim, evo_model, sess, input_data, conv_target, evo_target)
         print('Model run concluded.  Run time: {:5.3f} s.\n\n'.format(time.time()-t0))
+        return evo_output
     except KeyboardInterrupt:
         quit('Quit by KeyboardInterrupt.  Run time: {:5.3f} s.\n\n'.format(time.time()-t0))
 
 SMALL = 128
 BIG = 480
 SLEEP = .5
+NUM = 16
 
 def get_video_and_run():
     cap = cv2.VideoCapture(0)
+    updates = {
+        'one_img'           : False,
+        'batch_train_size'  : 16,
+        'num_conv1_filters' : 16,
+        'task'              : 'conv_task'
+    }
+    update_parameters(updates)
+    stim, evo_model, sess = load_model()
+
     while(True):
         # Capture frame-by-frame
         ret, frame = cap.read()
@@ -129,15 +116,25 @@ def get_video_and_run():
 
         # (16,128,128), (16,128,128,3) bw, (16,128,128,3) color
         # cv2.imshow('frame', gray)
-        gray = [gray] * 16
-        gray_3chan = [gray_3chan] * 16
-        frame = [frame] * 16
-        evo_output = actually_run_model(gray, gray_3chan, frame)
+        grey = gray.reshape(SMALL * SMALL)
+        gray = [grey] * NUM
+
+        grey_3chan = gray_3chan.reshape(SMALL * SMALL * 3)
+        gray_3chan = [grey_3chan] * NUM
+
+        freme = frame.reshape(SMALL * SMALL * 3)
+        frame = [freme] * NUM
+
+        evo_output = actually_run_model(stim, evo_model, sess, gray, gray_3chan, frame)[0]
+        evo_output = evo_output.reshape((SMALL,SMALL,3))
         # Display the resulting frame
         evo_output = cv2.resize(evo_output, (BIG, BIG))
+        print("evo_output after resize")
+        print(evo_output.shape)
         cv2.imshow('frame',evo_output)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
+        # break
 
         # sleep(SLEEP)
 
