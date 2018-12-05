9bd7cee9d1aeb32d6790c46810655f7208a530c0
Chaihyun Lee <clee0505@uchicago.edu>
2018-12-04 21:36:03 -0600

    lighter video streaming

--- best_evo.py
+++ best_evo.py
@@ -2,55 +2,61 @@
 Catherine (Chaihyun) Lee & Lucy Newman (2018)
 Contributions from Rick Stevens
 """
-from evo_utils import *
+from parameters import *
+from collections import OrderedDict
+import numpy as np
+
+def relu(x):
+    return np.maximum(0., x, dtype=x.dtype)
+
+def pad(x):
+    shape = x.shape
+    img_size = shape[-2]
+
+    temp = np.zeros((img_size+2,img_size+2,shape[-1]))
+    temp[:img_size,:img_size,:] = x
+    return temp
+
+def apply_filter(x, filt):
+    img_size = x.shape[-2] - 2
+
+    temp = np.zeros((img_size,img_size))
+    for i in range(img_size):
+        for j in range(img_size):
+            temp[i,j] = np.sum(x[i:i+3,j:j+3,:] * filt)
+    
+    return temp
+
+def convolve(x, var_dict, filt_type):
+    conv = np.zeros((*par['inp_img_shape'],3))
+        
+    i = 0
+    x = pad(x)
+    for key in var_dict.keys():
+        if filt_type in key:
+            conv[:,:,i] = apply_filter(x, var_dict[key])
+            i += 1
+
+    return conv
+
 
 class EvoModel:
 
     def __init__(self):
         
-        self.make_constants()
-        self.original = True
-        self.var_dict = {}
+        self.var_dict = OrderedDict()
  
     def update_variables(self, updates):
         for key, val in updates.items():
-            self.var_dict[key] = to_gpu(val)
-
-    def make_constants(self):
-        constants = ['n_networks','mutation_rate','mutation_strength','cross_rate']
-
-        self.con_dict = {}
-        for c in constants:
-            self.con_dict[c] = to_gpu(par[c])
+            self.var_dict[key] = val
 
     def load_batch(self, input_data, target_data):
-        self.input_data = to_gpu(input_data)
-        self.target_data = to_gpu(target_data)
+        self.input_data = input_data
+        self.target_data = target_data
 
     def run_models(self):
         conv1 = relu(convolve(self.input_data, self.var_dict, 'conv2_filter') + self.var_dict['conv2_bias'])
-        self.output = cp.reshape(conv1, (par['batch_train_size'],*par['out_img_shape']))
-
-    def judge_models(self):
-        img_len = par['img_size']
-        self.target_data = cp.reshape(self.target_data, (par['batch_train_size'],*par['out_img_shape']))
-        trimmed_img = self.target_data[:,1:img_len-1,1:img_len-1,:]
-        self.loss = cp.mean(cp.square(trimmed_img - self.output[:,1:img_len-1,1:img_len-1,:])).astype(cp.float64)
-        self.output = cp.reshape(self.output, (par['batch_train_size'],par['n_output']))
-
-    def get_losses(self, ranked=True):
-        return to_cpu(self.loss)
-
-
-
-
-
-
-
-
-
-
-
+        self.output = np.reshape(conv1, (par['out_img_shape']))
 
 
 
--- video.py
+++ video.py
@@ -1,7 +1,4 @@
-import numpy as np
-import cv2
 from time import sleep
-
 import tensorflow as tf
 import cv2
 import pickle
@@ -9,11 +6,10 @@ import time
 import numpy as np
 np.set_printoptions(precision=3)
 import copy
-from stimulus import Stimulus
+# from stimulus import Stimulus
 import matplotlib.pyplot as plt
 # plt.switch_backend('agg')
 from parameters import *
-from evo_utils import *
 from best_evo import EvoModel
 
 # Ignore tensorflow warning
@@ -25,7 +21,7 @@ def load_model():
     tf.reset_default_graph()
 
     # Generate stimulus
-    stim = Stimulus()
+    stim = None #Stimulus()
     evo_model = EvoModel()
 
     saved_evo_model = pickle.load(open('./savedir/conv_task/run_14_model_stats.pkl','rb'))
@@ -40,48 +36,28 @@ def load_model():
     init = tf.global_variables_initializer()
     sess.run(init)
 
-    folder = './latent_all_img_batch16_filt16_loss150/'
+    folder = './latent_big_img_batch16_filt16_loss150/'
     conv_model = tf.train.import_meta_graph(folder + 'conv_model_with_latent.meta', clear_devices=True)
     conv_model.restore(sess, tf.train.latest_checkpoint(folder)) 
     print('Loaded conv model from',folder)
 
     return stim, evo_model, sess
 
-def run_model(stim, evo_model, sess, input_data, conv_target, evo_target):    
-
-    # Generate batch from testing set and check the output
-    # (16,128,128), (16,128,128,3) bw, (16,128,128,3) color
-    input_data, conv_target, evo_target = stim.generate_test_batch()
-    # for i in range(10):
-        # start = time.time()
-    feed_dict = {'x:0': input_data, 'y:0': conv_target}
-    test_loss, conv_output, encoded = sess.run(['l:0', 'o:0','encoded:0'], feed_dict=feed_dict)
-    
-
-    # "TEST" EVO MODEL
-    evo_model.load_batch(encoded, evo_target)
-    evo_model.run_models()
-    evo_model.judge_models()
-
-    evo_output = evo_model.output
-    test_loss = evo_model.get_losses(True)
-    print('Evo loss:',test_loss)
-    print((evo_output.shape, conv_output.shape))
-    return evo_output
-                # end = time.time()
-                # print(end-start)
-            # display evo_output[0]
 
 def actually_run_model(stim, evo_model, sess, input_data, conv_target, evo_target):
-    # Run Model
+
+    # input_data, conv_target, evo_target = stim.generate_test_batch()
+    feed_dict = {'x:0': input_data, 'y:0': conv_target}
+    conv_output, encoded = sess.run(['o:0','encoded:0'], feed_dict=feed_dict)
+    
+    # "TEST" EVO MODEL
     t0 = time.time()
-    try:
-        # print('Model number ' + str(par['run_number']) + ' running!')
-        evo_output = run_model(stim, evo_model, sess, input_data, conv_target, evo_target)
-        print('Model run concluded.  Run time: {:5.3f} s.\n\n'.format(time.time()-t0))
-        return evo_output
-    except KeyboardInterrupt:
-        quit('Quit by KeyboardInterrupt.  Run time: {:5.3f} s.\n\n'.format(time.time()-t0))
+    evo_model.load_batch(encoded[0], evo_target[0])
+    evo_model.run_models()
+
+    print('Model run concluded.  Run time: {:5.3f} s.\n\n'.format(time.time()-t0))
+    return evo_model.output
+
 
 SMALL = 128
 BIG = 480
@@ -105,7 +81,7 @@ def get_video_and_run():
 
         h, w = frame.shape[:2]
         frame = cv2.resize(frame, ((w * SMALL) // h, SMALL))
-        new_w = frame.shape[1]
+        new_w = frame.shape[1]-1
         off = (new_w - SMALL) // 2
         frame = np.array([row[off:new_w-off] for row in frame])
 
@@ -125,12 +101,9 @@ def get_video_and_run():
         freme = frame.reshape(SMALL * SMALL * 3)
         frame = [freme] * NUM
 
-        evo_output = actually_run_model(stim, evo_model, sess, gray, gray_3chan, frame)[0]
-        evo_output = evo_output.reshape((SMALL,SMALL,3))
+        evo_output = actually_run_model(stim, evo_model, sess, gray, gray_3chan, frame).astype(np.uint8)
         # Display the resulting frame
         evo_output = cv2.resize(evo_output, (BIG, BIG))
-        print("evo_output after resize")
-        print(evo_output.shape)
         cv2.imshow('frame',evo_output)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
